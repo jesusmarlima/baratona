8c06ae789215b77f9c22691f314b148225887344
Jesusmar Lima <jesusmarlima@gmail.com>
2016-11-23 23:02:26 -0500

    Make test less flaky

--- spec/features/bars_list_spec.rb
+++ spec/features/bars_list_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
-feature "Bar list" do
+feature "Bar list", js: true do
   scenario "View all bars in Park Slope" do
     visit '/'
-    expect(page).to have_content "Double Windsor"
+    expect(page).to have_content "Park Slope Bars"
   end
 end
