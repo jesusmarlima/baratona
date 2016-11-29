706c0023cdb3cc082d4052e8cf7c57d6eaf3760e
Jesusmar Lima <jesusmarlima@gmail.com>
2016-11-28 22:52:18 -0500

    allow moving bars out of selected list

--- app/assets/javascripts/components/App.es6.jsx
+++ app/assets/javascripts/components/App.es6.jsx
@@ -14,11 +14,28 @@ class App extends React.Component {
     })
   }
 
-  handleBarIncludeSearch(bar){
-    if (this.state.selected_bars.includes(bar)){
+  handleBarExcludeSearch(bar){
+    if (this.state.bars.find( (selected_bar)=>
+      selected_bar.yelp_id == bar.yelp_id
+    )){
         return
     }
+    this.state.selected_bars.splice(this.state.selected_bars.indexOf(bar), 1)
     this.setState({
+      bars: [bar].concat(this.state.bars),
+      selected_bars: this.state.selected_bars
+    })
+  }
+
+  handleBarIncludeSearch(bar){
+    if (this.state.selected_bars.find( (selected_bar)=>
+      selected_bar.yelp_id == bar.yelp_id
+    )){
+        return
+    }
+    this.state.bars.splice(this.state.bars.indexOf(bar), 1)
+    this.setState({
+      bars: this.state.bars,
       selected_bars: [bar].concat(this.state.selected_bars)
     })
   }
@@ -32,7 +49,7 @@ class App extends React.Component {
                   </div>
                   <div className="row">
                     <BarList bars = {this.state.bars} add_list={this.handleBarIncludeSearch.bind(this)} title="Bar List"/>
-                      <BarList bars = {this.state.selected_bars} title="Selected Bars"/>
+                      <BarList bars = {this.state.selected_bars} add_list={this.handleBarExcludeSearch.bind(this)} title="Selected Bars"/>
                   </div>
             </div>
         )
